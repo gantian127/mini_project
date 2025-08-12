cfc05f3de1553f369afb81e4f6f69f064f733f30
gantian127 <jamy127@foxmail.com>
2025-08-12 12:05:33 -0600

    clean code

--- flood_simulator.py
+++ flood_simulator.py
@@ -36,14 +36,8 @@ from landlab.components import OverlandFlow, SoilInfiltrationGreenAmpt
 
 
 class FloodSimulator:
-    def __init__(self,
-                 terrain,
-                 output,
-                 model_run,
-                 infil_info,
-                 olf_info):
-
-        """ Initialize FloodSimulator """
+    def __init__(self, terrain, output, model_run, infil_info, olf_info):
+        """Initialize FloodSimulator"""
 
         self.terrain = terrain
         self.output = output
@@ -61,7 +55,7 @@ class FloodSimulator:
 
     @classmethod
     def from_file(cls, config_file):
-        with open(config_file, mode='rb') as fp:
+        with open(config_file, mode="rb") as fp:
             args = tomllib.load(fp)
         return cls(**args)
 
@@ -69,52 +63,58 @@ class FloodSimulator:
         """create RasterModelGrid and add data fields"""
 
         # DEM field and set boundary condition
-        self.model_grid, dem_data = read_esri_ascii(self.terrain['grid_file'],
-                                                    name='topographic__elevation')
+        self.model_grid, dem_data = read_esri_ascii(
+            self.terrain["grid_file"], name="topographic__elevation"
+        )
 
-        if self.terrain['outlet_id'] < 0:
+        if self.terrain["outlet_id"] < 0:
             id_array = self.model_grid.set_watershed_boundary_condition(
-                                    dem_data,
-                                    nodata_value=self.terrain['nodata_value'],
-                                    return_outlet_id=True)
+                dem_data,
+                nodata_value=self.terrain["nodata_value"],
+                return_outlet_id=True,
+            )
             self.outlet_id = id_array[0]
         else:
-            self.outlet_id = self.terrain['outlet_id']
+            self.outlet_id = self.terrain["outlet_id"]
             self.model_grid.set_watershed_boundary_condition_outlet_id(
-                                    outlet_id=self.outlet_id,
-                                    node_data=dem_data,
-                                    nodata_value=self.terrain['nodata_value'])
+                outlet_id=self.outlet_id,
+                node_data=dem_data,
+                nodata_value=self.terrain["nodata_value"],
+            )
 
         # surface water depth TODO: allow tif input
-        self.model_grid.add_full("surface_water__depth",
-                                 self.olf_info['surface_water_depth'])
+        self.model_grid.add_full(
+            "surface_water__depth", self.olf_info["surface_water_depth"]
+        )
 
         # soil water infiltration depth  TODO: allow tif input
-        self.model_grid.add_full("soil_water_infiltration__depth",
-                                 self.infil_info['soil_water_infiltration_depth'])
+        self.model_grid.add_full(
+            "soil_water_infiltration__depth",
+            self.infil_info["soil_water_infiltration_depth"],
+        )
 
         # maximum surface water depth (this field is added for result analysis)
-        self.model_grid.add_zeros('max_surface_water__depth', at='node')
+        self.model_grid.add_zeros("max_surface_water__depth", at="node")
 
         # maximum discharge (this field is added for result analysis)
-        self.model_grid.add_zeros('test_max_discharge', at='node')
+        self.model_grid.add_zeros("test_max_discharge", at="node")
 
         # add rain intensity TODO: allow 3D rain input
-        if self.olf_info['rain_file'] != '':
-            file = rasterio.open(self.olf_info['rain_file'])
+        if self.olf_info["rain_file"] != "":
+            file = rasterio.open(self.olf_info["rain_file"])
             data = file.read(1).flatten()
             self.rain_intensity = data
         else:
-            self.rain_intensity = self.olf_info['rain_intensity']
+            self.rain_intensity = self.olf_info["rain_intensity"]
 
         # add hydraulic conductivity
-        if self.infil_info['conductivity_file'] != '':
-            file = rasterio.open(self.infil_info['conductivity_file'])
+        if self.infil_info["conductivity_file"] != "":
+            file = rasterio.open(self.infil_info["conductivity_file"])
             flip_data = np.flipud(file.read(1))  # make sure to flip the tiff file data
             data = flip_data.flatten()
             self.hydraulic_conductivity = data
         else:
-            self.hydraulic_conductivity = self.infil_info['hydraulic_conductivity']
+            self.hydraulic_conductivity = self.infil_info["hydraulic_conductivity"]
 
     def run(self):
         """
@@ -122,34 +122,37 @@ class FloodSimulator:
         """
 
         # set model run parameters
-        model_run_time = self.model_run['model_run_time'] * 60  # duration of run (s)
-        storm_duration = self.model_run['storm_duration'] * 60  # duration of rain (s)
-        time_step = self.model_run['time_step'] * 60
+        model_run_time = self.model_run["model_run_time"] * 60  # duration of run (s)
+        storm_duration = self.model_run["storm_duration"] * 60  # duration of rain (s)
+        time_step = self.model_run["time_step"] * 60
         elapsed_time = 0.0
 
         # output setup
         outlet_times = []
         outlet_discharge = []
-        output_folder = self.output['output_folder']
+        output_folder = self.output["output_folder"]
 
         if not os.path.isdir(output_folder):
             os.mkdir(output_folder)
 
         # instantiate overland flow component
-        overland_flow = OverlandFlow(self.model_grid,
-                                     steep_slopes=self.olf_info['steep_slopes'],
-                                     alpha=self.olf_info['alpha'],
-                                     mannings_n=self.olf_info['mannings_n'],
-                                     g=self.olf_info['g'],
-                                     theta=self.olf_info['theta'],
-                                     )
+        overland_flow = OverlandFlow(
+            self.model_grid,
+            steep_slopes=self.olf_info["steep_slopes"],
+            alpha=self.olf_info["alpha"],
+            mannings_n=self.olf_info["mannings_n"],
+            g=self.olf_info["g"],
+            theta=self.olf_info["theta"],
+        )
 
         # instantiate infiltration component
-        if self.model_run['activate_inf']:
+        if self.model_run["activate_inf"]:
             # update parameter values
-            for var_name in ['soil_pore_size_distribution_index',
-                             'soil_bubbling_pressure',
-                             'wetting_front_capillary_pressure_head']:
+            for var_name in [
+                "soil_pore_size_distribution_index",
+                "soil_bubbling_pressure",
+                "wetting_front_capillary_pressure_head",
+            ]:
                 if self.infil_info[var_name] is False:
                     self.infil_info[var_name] = None
 
@@ -157,20 +160,25 @@ class FloodSimulator:
             infiltration = SoilInfiltrationGreenAmpt(
                 self.model_grid,
                 hydraulic_conductivity=self.hydraulic_conductivity,
-                soil_bulk_density=self.infil_info['soil_bulk_density'],
+                soil_bulk_density=self.infil_info["soil_bulk_density"],
                 initial_soil_moisture_content=self.infil_info[
-                    'initial_soil_moisture_content'],
-                soil_type=self.infil_info['soil_type'],
+                    "initial_soil_moisture_content"
+                ],
+                soil_type=self.infil_info["soil_type"],
                 volume_fraction_coarse_fragments=self.infil_info[
-                    'volume_fraction_coarse_fragments'],
-                coarse_sed_flag=self.infil_info['coarse_sed_flag'],
+                    "volume_fraction_coarse_fragments"
+                ],
+                coarse_sed_flag=self.infil_info["coarse_sed_flag"],
                 surface_water_minimum_depth=self.infil_info[
-                    'surface_water_minimum_depth'],
+                    "surface_water_minimum_depth"
+                ],
                 soil_pore_size_distribution_index=self.infil_info[
-                    'soil_pore_size_distribution_index'],
-                soil_bubbling_pressure=self.infil_info['soil_bubbling_pressure'],
+                    "soil_pore_size_distribution_index"
+                ],
+                soil_bubbling_pressure=self.infil_info["soil_bubbling_pressure"],
                 wetting_front_capillary_pressure_head=self.infil_info[
-                    'wetting_front_capillary_pressure_head']
+                    "wetting_front_capillary_pressure_head"
+                ],
             )
 
         # run model simulation
@@ -182,14 +190,16 @@ class FloodSimulator:
 
                 # set rainfall intensity
                 if elapsed_time < storm_duration:
-                    overland_flow.rainfall_intensity = self.rain_intensity/(1000 * 3600)
+                    overland_flow.rainfall_intensity = self.rain_intensity / (
+                        1000 * 3600
+                    )
                 else:
                     overland_flow.rainfall_intensity = 0.0
 
                 # run model
                 overland_flow.overland_flow(dt=overland_flow.dt)
 
-                if self.model_run['activate_inf']:
+                if self.model_run["activate_inf"]:
                     infiltration.run_one_step(overland_flow.dt)
 
                 # update elapsed time
@@ -198,7 +208,7 @@ class FloodSimulator:
                 # get discharge result at outlet
                 discharge = overland_flow.discharge_mapper(
                     self.model_grid.at_link["surface_water__discharge"],
-                    convert_to_volume=True
+                    convert_to_volume=True,
                 )
 
                 outlet_discharge.append(discharge[self.outlet_id])
@@ -221,7 +231,7 @@ class FloodSimulator:
             #     self.model_grid.at_node['surface_water__depth'])
 
             # plot overland flow results
-            if self.output['plot_olf']:
+            if self.output["plot_olf"]:
                 fig, ax = plt.subplots(
                     2, 1, figsize=(8, 9), gridspec_kw={"height_ratios": [1, 1.5]}
                 )
@@ -239,14 +249,14 @@ class FloodSimulator:
                     var_name="surface water depth (m)",
                 )
                 ax[1].set_title("")
-                ax[1].set_xlabel('east-west distance (m)')
-                ax[1].set_ylabel('north-south distance (m)')
+                ax[1].set_xlabel("east-west distance (m)")
+                ax[1].set_ylabel("north-south distance (m)")
 
                 plt.close(fig)
                 fig.savefig(os.path.join(output_folder, f"flow_{time_slice}.png"))
 
             # plot infiltration result
-            if self.model_run['activate_inf'] and self.output['plot_inf']:
+            if self.model_run["activate_inf"] and self.output["plot_inf"]:
                 fig, ax = plt.subplots(figsize=(8, 6))
                 imshow_grid(
                     self.model_grid,
@@ -255,19 +265,25 @@ class FloodSimulator:
                     var_name=" soil water infiltration depth (m)",
                 )
                 ax.set_title("")
-                ax.set_xlabel('east-west distance (m)')
-                ax.set_ylabel('north-south distance (m)')
+                ax.set_xlabel("east-west distance (m)")
+                ax.set_ylabel("north-south distance (m)")
 
                 plt.close(fig)
                 fig.savefig(os.path.join(output_folder, f"infil_{time_slice}.png"))
 
         # save outlet discharge
-        outlet_result = pd.DataFrame(list(zip(outlet_times, outlet_discharge)),
-                                     columns=['time', 'discharge'])
-        outlet_result.to_csv(os.path.join(
-            self.output['output_folder']
-            if os.path.isdir(self.output['output_folder']) else os.getcwd(),
-            'outlet_discharge.csv')
+        outlet_result = pd.DataFrame(
+            list(zip(outlet_times, outlet_discharge)), columns=["time", "discharge"]
+        )
+        outlet_result.to_csv(
+            os.path.join(
+                (
+                    self.output["output_folder"]
+                    if os.path.isdir(self.output["output_folder"])
+                    else os.getcwd()
+                ),
+                "outlet_discharge.csv",
+            )
         )
 
 
@@ -282,4 +298,4 @@ if __name__ == "__main__":
         fs = FloodSimulator.from_file(config_file)
         fs.run()
     else:
-        print('Please provide a configuration file path to run the model.')
+        print("Please provide a configuration file path to run the model.")
